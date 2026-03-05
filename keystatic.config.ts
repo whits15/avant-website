fb466f67ad20dceb424204862f1c1415b6f6a073
whits15 <80317938+whits15@users.noreply.github.com>
2026-03-05 15:06:21 -0500

    fix: update Keystatic Cloud project slug to landship/avant

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -5,7 +5,7 @@ export default config({
         kind: "cloud",
     },
     cloud: {
-        project: "landship/avant-website",
+        project: "landship/avant",
     },
     collections: {
         posts: collection({
